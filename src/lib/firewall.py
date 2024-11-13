c2d240bc5516fb3e1286091d6b69a2f6127532ee
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-13 19:51:02 -0300

    no importa

--- src/lib/firewall.py
+++ src/lib/firewall.py
@@ -6,40 +6,40 @@ Coursera:
 Professor: Nick Feamster
 Teaching Assistant: Arpit Gupta
 '''
-
-from pox.core import core
-import pox.openflow.libopenflow_01 as of
-from pox.lib.revent import *
-from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr
-from collections import namedtuple
 import os
+
+from collections import namedtuple
+from pox.pox.lib.addresses import EthAddr
+from pox.pox.lib.util import dpidToStr
+from pox.pox.lib.revent import *
+import pox.pox.openflow.libopenflow_01 as of
+from pox.pox.core import core
+
 ''' Add your imports here ... '''
 
 
-
 log = core.getLogger()
-policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]
+policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ['HOME']
 
 ''' Add your global variables here ... '''
 
 
-
 class Firewall (EventMixin):
 
-    def __init__ (self):
+    def __init__(self):
         self.listenTo(core.openflow)
         log.debug("Enabling Firewall Module")
 
-    def _handle_ConnectionUp (self, event):
+    def _handle_ConnectionUp(self, event):
         ''' Add your logic here ... '''
-
-
 
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
-def launch ():
+
+def launch():
     '''
     Starting the Firewall module
     '''
     core.registerNew(Firewall)
+
+    print("NO ROMPI")
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from lib.firewall import launch
+
+
+def main():
+    launch()
+
+
+if __name__ == "__main__":
+    main()
